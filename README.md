b6105e14d69a4f2e8a63b54b8bdbf927bf3ee2ed
unknown <jruttercorbett@outlook.com>
2020-12-05 18:52:38 +0000

    remake

--- README.md
+++ README.md
@@ -11,13 +11,13 @@
 
 ##  Images:
 ### Index Page with highly rated films
-![image](https://user-images.githubusercontent.com/70725429/96537945-5758e900-128f-11eb-929a-f79ab7d2ba45.png)
+![image](https://user-images.githubusercontent.com/70725429/101260327-0447d200-3727-11eb-9b4e-6774c28756ae.png)
 
 ### Movie search view
-![image](https://user-images.githubusercontent.com/70725429/96538267-19a89000-1290-11eb-8112-992feb7219c4.png)
+![image](https://user-images.githubusercontent.com/70725429/101260341-16297500-3727-11eb-8a90-98fbe2bfead7.png)
 
 ### Individual movie view with corresponding information
-![image](https://user-images.githubusercontent.com/70725429/96538417-62f8df80-1290-11eb-8289-f863148d4e25.png)
+![image](https://user-images.githubusercontent.com/70725429/101260353-22adcd80-3727-11eb-894c-942ded4a64e8.png)
 
 ### mobile view
-![image](https://user-images.githubusercontent.com/70725429/96538612-d39ffc00-1290-11eb-8c8f-673c00c0f473.png)
+![image](https://user-images.githubusercontent.com/70725429/101260355-2f322600-3727-11eb-88e4-e424ffc61487.png)
--- Scripts/movieSearch.js
+++ Scripts/movieSearch.js
@@ -1,102 +1,114 @@
 $(document).ready(function() {
-    //shows a bunch of movies on page load
 
-    $.get("assets/ID.txt", function(data){
+    //changes navbar colour + contents when scrolled
+    $(window).scroll(function() {
+        $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 250);
+    })
+
+    //shows a bunch of movies on page load
+    $.get("assets/ID.txt", function(data) {
 
         IdData = data.split("\n");
         lines = data.replace(/\r\n|\r/g, '\n').trim().split('\n');
         line = lines.slice(0, 100);
 
-      $.each(line, function(key, value){
-          const movieLoad = "https://www.omdbapi.com/?apikey=60ec64a7&i=" + value;
-          $.ajax(movieLoad).then(function(data){
-              var output = "";
-              //if there are no images available then it will use a fallback image
-              var poster = (data.Poster == 'N/A') ? 'assets/images/noImg.png' : data.Poster; 
-              output += `
-              <div class="movie">
-              <a onclick="movieSelected('${data.imdbID}')" khref="#"><img src="${poster}" alt="W3Schools.com"></a>
-              <a onclick="movieSelected('${data.imdbID}')" class="Select_button" href="#">${data.Title}</a>
+        $.each(line, function(key, value) {
+            const movieLoad = "https://www.omdbapi.com/?apikey=60ec64a7&i=" + value;
+            $.ajax(movieLoad).then(function(data) {
+                var output = "";
+                //if there are no images available then it will use a fallback image
+                var poster = (data.Poster == 'N/A') ? 'assets/images/noImg.png' : data.Poster;
+                output += `
+              <div class="card-group rounded">
+                <div class="card card_poster">
+                    <a onclick="movieSelected('${data.imdbID}')" khref="#"><img class="card-img-top" src="${poster}" alt="${data.Title} poster"></a>
+                </div>
               </div>
               `;
-              
-              $("#list").append(output);
+
+                $("#list").append(output);
             });
-          
-      });
+
+        });
 
     });
 
     // this is the movie search
 
-    $('#movieSearchBar').on('submit', function(event){
-        const title =  $('#searchMovie').val(); 
+    $('#movieSearchBar').on('submit', function(event) {
+        const title = $('#searchMovie').val();
         getMovies(title);
         event.preventDefault();
         $('#searchMovie').val("");
     });
 
-    function getMovies(title){
+    function getMovies(title) {
         const movieURL = "https://www.omdbapi.com/?apikey=60ec64a7&s=" + title;
-        
-        $.ajax(movieURL).then(function(data){
+
+        $.ajax(movieURL).then(function(data) {
             var error = data.Response;
-            if (error == 'False'){
+            if (error == 'False') {
                 $("#list").html(data.Error);
             } else {
                 var output = "";
                 let movies = data.Search;
-            $.each(movies, (index, movie) => {
-                var poster = (movie.Poster == 'N/A') ? 'assets/images/noImg.png' : movie.Poster; 
-                output += `
-                <div class="movie">
-                <a onclick="movieSelected('${movie.imdbID}')" khref="#"><img src="${poster}" alt="W3Schools.com"></a>
-                <a onclick="movieSelected('${movie.imdbID}')" class="Select_button" href="#">${movie.Title}</a>
+                $.each(movies, (index, movie) => {
+                    const movieID = "https://www.omdbapi.com/?apikey=60ec64a7&i=" + movie.imdbID;
+                    $.ajax(movieID).then(function(movie) {
+                        var poster = (movie.Poster == 'N/A') ? 'assets/images/noImg.png' : movie.Poster;
+                        output += `
+                <div class="card-group rounded">
+                    <div class="card card_poster">
+                        <a onclick="movieSelected('${movie.imdbID}')" khref="#"><img class="card-img-top" src="${poster}" alt="${data.Title} poster"></a>
+                    </div>
                 </div>
                 `;
-              });
-            $("#list").html(output);
+                        $("#list").html(output);
+                    });
+                });
             }
         });
-      
-        
     }
-    
+
 
 });
 //function for getting the ID for the individual movie
-function movieSelected(id){
+function movieSelected(id) {
     sessionStorage.setItem('movieId', id);
     window.location = 'movie.html';
     return false;
 }
 //gets the individual movie and displays it on a page
-function getMovie(){
+function getMovie() {
     let movieId = sessionStorage.getItem('movieId');
 
     const find = "https://www.omdbapi.com/?apikey=60ec64a7&i=" + movieId;
-  
-    $.ajax(find).then(function(data){
+
+    $.ajax(find).then(function(data) {
         let movie = data;
-        let output =`
-        <title>${movie.Title}</title>
-        <div class="movieContainer">
-        <img src="${movie.Poster}">
-        <div class="column">
-        <h1>${movie.Title}</h1>
-        <p>${movie.Plot}</p>
-        <ul class="w3-ul">
-              <li><strong class="listTitle">Genre:</strong> ${movie.Genre}</li>
-              <li><strong class="listTitle">Released:</strong> ${movie.Released}</li>
-              <li><strong class="listTitle">Rated:</strong> ${movie.Rated}</li>
-              <li><strong class="listTitle">IMDB Rating:</strong> ${movie.imdbRating}</li>
-              <li><strong class="listTitle">Director:</strong> ${movie.Director}</li>
-              <li><strong class="listTitle">Writer:</strong> ${movie.Writer}</li>
-              <li><strong class="listTitle">Actors:</strong> ${movie.Actors}</li>
-        </ul>
-        </div>
-        </div>
+        let output = `
+    <div class="card mb-3 mt-5 p-2 info_card" style="max-width: 750px;">
+		<div class="row no-gutters">
+			<div class="col-md-4"><img alt="${movie.Title} Image" class="card-img" src="${movie.Poster}"></div>
+			<div class="col-md-8">
+				<div class="card-body">
+					<h5 class="card-title pb-3 text-center">${movie.Title}</h5>
+					<p class="pb-3 text-center">${movie.Plot}</p>
+					<ul class="list-group list-group">
+						<li class="list-group-item" style="height: auto;">Genre - ${movie.Genre}</li>
+						<li class="list-group-item" style="height: auto;">Release Year - ${movie.Released}</li>
+						<li class="list-group-item" style="height: auto;">Age Rating - ${movie.Rated}</li>
+						<li class="list-group-item" style="height: auto;">IMDb Rating - ${movie.imdbRating}</li>
+						<li class="list-group-item" style="height: auto;">Writer(s) - ${movie.Writer}</li>
+						<li class="list-group-item" style="height: auto;">Director - ${movie.Director}</li>
+						<li class="list-group-item" style="height: auto;">Actors - ${movie.Actors}</li>
+						<li class="list-group-item" style="height: auto;">Awards - ${movie.Awards}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div
         `;
         $('#movie').html(output);
     });
-    }
+}
